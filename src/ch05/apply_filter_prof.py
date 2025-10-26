7b6d7284c9bcc7424f6823a93ec8737054a4a423
SY-256 <yamamotosw1005@gmail.com>
2025-10-26 22:12:57 +0900

    10/26 commit

---
+++ src/ch05/apply_filter_prof.py
@@ -0,0 +1,18 @@
+import cProfile
+import pstats
+import pyximport
+
+import numpy as np
+from PIL import Image
+
+pyximport.install(setup_args={"include_dirs": np.get_include()})
+
+import cyfilter_prof as cyfilter
+
+image = Image.open("../ch04/aurora.jpg")
+gray_filter = Image.open("./filter.png").convert("L")
+image_arr, gray_arr = np.array(image), np.array(gray_filter)
+
+cProfile.run("cyfilter.darken_annotated(image_arr, gray_arr)", "apply_filter.prof")
+s = pstats.Stats("apply_filter.prof")
+s.strip_dirs().sort_stats("time").print_stats()
